c234ffe3870401c06b1e9d567fdf78ab3817abce
Sonia Keys <soniakeys@gmail.com>
2014-09-10 19:54:43 -0400

    Nix Mendel1. Sexual reproduction with one sex is too weird.

--- probability.go
+++ probability.go
@@ -1,27 +1,5 @@
 package bio
 
-// Mendel1 computes offspring genotype probabilities by Mendel's first
-// law, the law of segregation.
-//
-// Inputs are the number individuals in a population which are dominant,
-// heterozygous, and recessive respectively for a specific trait.
-//
-// Returned are the probabilities of an offspring being dominant,
-// heterozygous, or recessive for the trait.
-func Mendel1(dom, het, rec int) (pd, ph, pr float64) {
-	dd := float64(dom * (dom - 1))
-	hh := float64(het * (het - 1))
-	rr := float64(rec * (rec - 1))
-	dh := float64(dom * het)
-	hr := float64(het * rec)
-	rd := float64(rec * dom)
-	pop := dom + het + rec // total population
-	denom := float64(pop * (pop - 1))
-	return (dd + dh + hh*.25) / denom,
-		(rd*2 + dh + hr + hh*.5) / denom,
-		(rr + hr + hh*.25) / denom
-}
-
 // Permute computes permutations of an integer slice.
 //
 // It takes a callback function that Permute calls on each permutation.
